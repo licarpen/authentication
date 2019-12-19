d128ea391dec1a933c50568527cc857171a117be
Lisa Carpenter <carpenter.lisa@gmail.com>
2019-12-19 14:29:19 -0800

    added signup route and tests

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -4,8 +4,9 @@ const request = require('supertest');
 const app = require('../lib/app');
 const connect = require('../lib/utils/connect');
 const mongoose = require('mongoose');
+const User = require('../lib/models/User');
 
-describe('app routes', () => {
+describe('app authentication routes', () => {
   beforeAll(() => {
     connect();
   });
@@ -14,7 +15,25 @@ describe('app routes', () => {
     return mongoose.connection.dropDatabase();
   });
 
+  /* beforeEach(async() => {
+    await User.create({ email: 'me@me.com', password: '123' });
+  });
+  */
+
   afterAll(() => {
     return mongoose.connection.close();
+  });
+
+  it('signs up a user', () => {
+    return request(app)
+      .post('/api/v1/auth/signup')
+      .send({ email: 'me@me.com', password: '123' })
+      .then(res => {
+        expect(res.body).toEqual({
+          _id: expect.any(String),
+          email: 'me@me.com',
+          __v: 0
+        });
+      });
   });
 });
